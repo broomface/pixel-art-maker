61c1f01b7a5a3acbbf000497fa225ced35289d18
nrfi@sbcglobal.net <scott.m.rushford@gmail.com>
2017-10-19 23:45:07 -0600

    Added Erase Button

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 
-var color = '';
+// canvas is the individual tile
+// container is my parent container
+// colorSpace is my box with all colors to be staged
+// board is class in container
+
+
+var color = '';  // holding the current value
 
     for(var i = 0; i < 3526; i++){
-      var canvas=document.createElement('div');
+      var canvas=document.createElement('div');  // esch tile
       canvas.className='canvas';
-      var container = document.getElementsByClassName('board')[0];
-      container.appendChild(canvas);
+      var board = document.getElementsByClassName('board')[0];
+      board.appendChild(canvas);  // parent to append to
     }
     var colorSpace = document.getElementsByClassName('colorSpace')[0];
     colorSpace.addEventListener("click", function(event){
-  color = event.target.style.backgroundColor;
+  color = event.target.style.backgroundColor;  //
    });
 
     var board = document.getElementsByClassName('board')[0]
